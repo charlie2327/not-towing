2728f543f01fb07743fd19c1ca7f189437ba2d8a
jflores_mvapp <juan@logacode.com>
2025-01-30 15:17:23 -0600

    add input quantity and description

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
-import './App.css';
+import "./App.css";
 import React, { useState } from "react";
-import { DatePicker, Button, Card, Typography, Space, Row, Col } from "antd";
+import {
+  DatePicker,
+  Button,
+  Card,
+  Typography,
+  Space,
+  Row,
+  Col,
+  Input,
+  
+} from "antd";
 import dayjs, { Dayjs } from "dayjs";
 import "antd/dist/reset.css";
 
@@ -13,7 +23,7 @@ function App() {
   const [selectedTime, setSelectedTime] = useState(null);
 
   return (
-<div style={{ maxWidth: '80%', margin: "auto", padding: 20 }}>
+    <div style={{ maxWidth: "80%", margin: "auto", padding: 20 }}>
       <Card bordered>
         <Row gutter={16} align="middle">
           {/* Información de la Cita */}
@@ -29,48 +39,54 @@ function App() {
               <p>Telefono del Conductor</p>
               <Text strong> Email:</Text>
               <p>Email del Conductor</p>
-              
-              
-
             </Card>
           </Col>
 
           {/* Selección de Fecha */}
           <Col span={12}>
-           <Card bordered>
-            <Title level={5}>Selecciona una fecha:</Title>
-            <DatePicker
-              onChange={(date) => setSelectedDate(date)}
-              disabledDate={(current) => current && current < dayjs().startOf("day")}
-              style={{ width: "100%" }}
-              format="DD/MM/YYYY"
-            />
-            {/* agregar input de cantidad y descripcion */}
+            <Card bordered>
+              <Text strong>Cantidad</Text>
+              <Input type="number" />
+              <Text strong>Selecciona una fecha:</Text>
+              <DatePicker
+                onChange={(date) => setSelectedDate(date)}
+                disabledDate={(current) =>
+                  current && current < dayjs().startOf("day")
+                }
+                style={{ width: "100%" }}
+                format="DD/MM/YYYY"
+              />
+              <Text strong>Descripción</Text>
+              <Input.TextArea
+                  type="textarea"
+
+                placeholder="Descripción de la carga"
+              />
             </Card>
           </Col>
 
           {/* Selección de Hora */}
           <Col span={6}>
-          <Card bordered>
-            <Title level={5}>Selecciona una hora:</Title>
-            <Space direction="vertical" style={{ width: "100%" }}>
-              {/* validar las horas en dependencia de la hora trasncurrida */}
-              {availableTimes.map((time) => (
-                <Button
-                  key={time}
-                  type={selectedTime === time ? "primary" : "default"}
-                  onClick={() => setSelectedTime(time)}
-                  style={{
-                    width: "100%",
-                    textAlign: "center",
-                    background: selectedTime === time ? "#1890ff" : "white",
-                    color: selectedTime === time ? "white" : "black",
-                  }}
-                >
-                  {time}
-                </Button>
-              ))}
-            </Space>
+            <Card bordered>
+              <Title level={5}>Selecciona una hora:</Title>
+              <Space direction="vertical" style={{ width: "100%" }}>
+                {/* validar las horas en dependencia de la hora trasncurrida */}
+                {availableTimes.map((time) => (
+                  <Button
+                    key={time}
+                    type={selectedTime === time ? "primary" : "default"}
+                    onClick={() => setSelectedTime(time)}
+                    style={{
+                      width: "100%",
+                      textAlign: "center",
+                      background: selectedTime === time ? "#1890ff" : "white",
+                      color: selectedTime === time ? "white" : "black",
+                    }}
+                  >
+                    {time}
+                  </Button>
+                ))}
+              </Space>
             </Card>
           </Col>
         </Row>
@@ -79,7 +95,9 @@ function App() {
         {selectedDate && selectedTime && (
           <Card style={{ marginTop: 20, textAlign: "center" }}>
             <Text strong>Cita seleccionada:</Text>
-            <p>{selectedDate.format("DD/MM/YYYY")} a las {selectedTime}</p>
+            <p>
+              {selectedDate.format("DD/MM/YYYY")} a las {selectedTime}
+            </p>
             <Button type="primary">Confirmar Cita</Button>
           </Card>
         )}
